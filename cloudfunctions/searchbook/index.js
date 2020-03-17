80c668af20cf53867220623b9d860b26f7cc68cf
td <578279246@qq.com>
2020-03-17 18:53:32 +0800

    改了下搜索引擎的代码，请蒋瀚锐过目

--- cloudfunctions/searchbook/index.js
+++ cloudfunctions/searchbook/index.js
@@ -1,12 +1,60 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 
-cloud.init()
+cloud.init("env: dinger-3lqz8")
 
+class Search {
+  constructor(book) {
+    this.book = book;
+  }
+  search(arg) {
+    var score = [];
+    for (var i = 0; i < book.length; i++) {
+      var temp = {
+        'id': this.book[i]['id'],
+        's': ((this.book[i]['name']).length),
+        'name': (this.book[i]['name'])
+      };
+      temp['s'] += this.important(arg, book[i]);
+      score.push(temp);
+    }
+    score.sort(function (a, b) {
+      if (a['s'] < b['s']) {
+        return -1;
+      } else if (a['s'] > b['s']) {
+        return 1;
+      } else {
+        return 0;
+      }
+    })
+    let sorted = [];
+    for (let i = 0; i < score.length; i++) {
+      if (score[i]['s'] < 0) {
+        sorted.push({ 'id': score[i]['id'], 'name': score[i]['name'] });
+      }
+    }
+    return sorted;
+  }
+  important(args, element) {
+    if (element['name'].match(args) != null) {
+      return -1000;
+    } else {
+      var mark = 0;
+      for (var x in args) {
+        if (element['name'].match(args) != null) {
+          mark -= 10;
+        }
+      }
+      return mark;
+    }
+  }
+}
 // 云函数入口函数
 exports.main = async (event, context) => {
   res = wx.cloud.database().collection('book').get();
+  info = new Search(res);
   return {
-    res
+    id: info.id,
+    name: info.name
   }
 }
