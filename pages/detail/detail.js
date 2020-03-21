9e7818f2f923eecf53b2f442a62c90e3f5700fae
Lam <401139729@qq.com>
2020-03-21 08:42:01 +0800

    gotodetail

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -6,29 +6,30 @@ Page({
    * 页面的初始数据
    */
   data: {
-    content: '',
-    pinglun: [],
+    // content: '',
+    // pinglun: [],
     detail: '',
-    goods_info: {
-      id: '123',
-      picture: [{
-          img: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3438576193,3301397209&fm=27&gp=0.jpg'
-        },
-        {
-          img: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1525546566,2404337493&fm=27&gp=0.jpg'
-        },
-        {
-          img: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3028702483,4182396631&fm=27&gp=0.jpg'
-        }
-      ],
-      title: "2020年二级建造师教材习题全套6本",
-      summary: "备注：正版 半新",
-      price: "30",
-      userava: "/images/demo.jpeg",
-      userid: "白银御行",
-      userlocation: "紫菘公寓",
-      favoriteimg: '/images/favoritepassive.png',
-    },
+    userlist:'',
+    // goods_info: {
+    //   id: '123',
+      // picture: [{
+      //     img: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3438576193,3301397209&fm=27&gp=0.jpg'
+      //   },
+      //   {
+      //     img: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1525546566,2404337493&fm=27&gp=0.jpg'
+      //   },
+      //   {
+      //     img: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3028702483,4182396631&fm=27&gp=0.jpg'
+      //   }
+      // ],
+      // title: "2020年二级建造师教材习题全套6本",
+      // summary: "备注：正版 半新",
+      // price: "30",
+      // userava: "/images/demo.jpeg",
+      // userid: "白银御行",
+      // userlocation: "紫菘公寓",
+    //   favoriteimg: '/images/favoritepassive.png',
+    // },
     //swiper相关
     indicatorDots: true,
     autoplay: true,
@@ -37,82 +38,93 @@ Page({
     circular: true,
     goods: {}
   },
-  favorite: function() {
-    this.setData({
-      favoriteimg: isClick ? '/images/favoritepassive.png' : '/images/myfavorite.png'
-    })
-    isClick = !isClick;
-    wx.cloud.callFunction({
-        name: "addfavorite",
-        data: {
-          action: "favorite",
-          id: ID,
-          isClick: isClick
-        }
-      })
-      .then(res => {
-        console.log("改变收藏状态成功", res)
-      })
-      .catch(res => {
-        console.log("改变收藏状态失败", res)
-      })
-  },
-  getcontent(event) {
-    this.setData({
-      content : event.detail.value
-    })
+  // favorite: function() {
+  //   this.setData({
+  //     favoriteimg: isClick ? '/images/favoritepassive.png' : '/images/myfavorite.png'
+  //   })
+  //   isClick = !isClick;
+  //   wx.cloud.callFunction({
+  //       name: "addfavorite",
+  //       data: {
+  //         action: "favorite",
+  //         id: ID,
+  //         isClick: isClick
+  //       }
+  //     })
+  //     .then(res => {
+  //       console.log("改变收藏状态成功", res)
+  //     })
+  //     .catch(res => {
+  //       console.log("改变收藏状态失败", res)
+  //     })
+  // },
+  // getcontent(event) {
+  //   this.setData({
+  //     content : event.detail.value
+  //   })
 
-    console.log("y用户输入的值", event)
-  },
-  fabiao() {
-    let newcontent = {}
-    let pinglunARR = this.data.pinglun
-    let content=this.data.content
-    newcontent.name = "王五"
-    newcontent.content = content
-    pinglunARR.push(newcontent)
-    wx.cloud.callFunction({
-        name: "addfavorite",
-        data: {
-          action: "comment",
-          id: ID,
-          pinglun: pinglunARR,
-        }
-      })
-      .then(res => {
-        console.log("评论成功", res)
-        this.setData({
-          pinglun: pinglunARR,
-          content:'',
-        })
-        wx.showToast({
-          title: '发表成功',
-        })
-      })
-      .catch(res => {
-        console.log("评论失败", res)
-      })
-  },
+  //   console.log("y用户输入的值", event)
+  // },
+  // fabiao() {
+  //   let newcontent = {}
+  //   let pinglunARR = this.data.pinglun
+  //   let content=this.data.content
+  //   newcontent.name = "王五"
+  //   newcontent.content = content
+  //   pinglunARR.push(newcontent)
+  //   wx.cloud.callFunction({
+  //       name: "addfavorite",
+  //       data: {
+  //         action: "comment",
+  //         id: ID,
+  //         pinglun: pinglunARR,
+  //       }
+  //     })
+  //     .then(res => {
+  //       console.log("评论成功", res)
+  //       this.setData({
+  //         pinglun: pinglunARR,
+  //         content:'',
+  //       })
+  //       wx.showToast({
+  //         title: '发表成功',
+  //       })
+  //     })
+  //     .catch(res => {
+  //       console.log("评论失败", res)
+  //     })
+  // },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad:function(options) {
     ID = options.id
-    console.log("详情页接受的id", ID)
-    wx.cloud.database().collection('homelist')
+    console.log(ID)
+    wx.cloud.database().collection('bookdata')
       .doc(ID)
       .get()
       .then(res => {
-        console.log("详情页请求成功", res)
+        console.log("详情页id请求成功", res)
         this.setData({
           detail: res.data,
-          favoriteimg: isClick ? '/images/favoritepassive.png' : '/images/myfavorite.png',
-          pinglun: res.data.plugin
+          // favoriteimg: isClick ? '/images/favoritepassive.png' : '/images/myfavorite.png',
+          // pinglun: res.data.plugin
         })
-        isClick = res.data.isClick
+        // isClick = res.data.isClick
       })
       .catch(res => {
-        console.log("详情页请求失败", res)
+        console.log("详情页id请求失败", res)
+      })
+    wx.cloud.database().collection('users')
+      .get()
+      .then(res => {
+        console.log("详情页users请求成功", res)
+        this.setData({
+          userlist: res.data,
+        })
+      })
+      .catch(res => {
+        console.log("详情页users请求失败", res)
       })
 
 
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,23 +1,75 @@
 // pages/home/home.js
+var leftdata = []; //左容器图片
+var rightdata = []; //右容器图片
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    inputvalue:'',
-    location: ["不限地点","沁苑公寓", "韵苑公寓", "紫菘公寓"],
+    inputvalue: '',
+    location: ["不限地点", "沁苑公寓", "韵苑公寓", "紫菘公寓"],
     index1: 0,
-    type: ["不限类型","文学书籍", "理科书籍", "工科书籍", "社科书籍", "医学书籍", "其他书籍"],
+    type: ["不限类型", "文学书籍", "理科书籍", "工科书籍", "社科书籍", "医学书籍", "其他书籍"],
     index2: 0,
-    datalist:[]
+    userlist:[],
+    datalist: [],
+    imgList: [],
+    dataLeft: [], //左容器图片
+    dataRight: [], //右容器图片
+    lHeight: 0, //左容器高
+    rHeight: 0, //右容器高
+    imgWidth: 0, //图片宽
   },
-inputBind:function(e){
+  imgLoad: function(e) {
+    let datalist=this.data.datalist;
+    let imgList = this.data.imgList;
+    //图片原始宽度
+    let beforeWidth = e.detail.width;
+    //图片原始高度
+    let beforeHeight = e.detail.height;
+    //图片显示的宽度
+    let nowWidth = this.data.imgWidth;
+    //比例=图片原始宽度/图片显示的宽度
+    let wProportion = beforeWidth / nowWidth;
+    //图片显示的高度=图片原始高度/比例
+    let imgHeight = beforeHeight / wProportion;
+// console.log("测试数据为",e)
+    //当左区域高=右区域高   或   当左区域高<右区域高
+    if (this.data.lHeight == this.data.rHeight || this.data.lHeight < this.data.rHeight) {
+      leftdata.push(datalist[e.target.dataset.index])
+      this.setData({
+        lHeight: this.data.lHeight + imgHeight
+      })
+      // console.log("左高",imgHeight)
+      //当左区域高>右区域高
+    } else if (this.data.lHeight > this.data.rHeight) {
+      rightdata.push(datalist[e.target.dataset.index])
+      this.setData({
+        rHeight: this.data.rHeight + imgHeight
+      })
+    }
+    //当完成最后一次分组时        
+    if (e.target.dataset.index == this.data.imgList.length - 1) {
+      this.setData({
+        dataLeft: leftdata,
+        dataRight: rightdata,
+        imgList: []
+      })
+    }
+  },
+  gotodetail:function(e){
+    console.log(e)
+    wx.navigateTo({
+      url: '/pages/detail/detail?id=' + e.currentTarget.dataset.item._id,
+    })
+  },
+  inputBind: function(e) {
     this.setData({
       inputvalue: e.detail.value
     })
-      console.log("输入数据为"+this.data.inputvalue)
+    console.log("输入数据为" + this.data.inputvalue)
   },
-  push:function(){
+  push: function() {
     var that = this
     wx.request({
       url: '' + this.data.inputvalue + /0/,
@@ -25,7 +77,7 @@ inputBind:function(e){
         inputvalue: this.data.inputvalue
       },
       method: 'GET',
-      success: function (res) {
+      success: function(res) {
         console.log(res.data)
         var searchData = res.data
         that.setData({
@@ -80,30 +132,30 @@ inputBind:function(e){
       }
     })
   },
-  getData: function () {
+  getData: function() {
     wx.cloud.callFunction({
       name: '',
       data: {
         type: 'get'
       },
-      success: function (res) {
+      success: function(res) {
         console.log("调用成功");
         // 停止下拉动作
         wx.stopPullDownRefresh();
-    },
-      fail: function () {
+      },
+      fail: function() {
         console.log("调用失败");
         wx.stopPullDownRefresh();
       }
     })
-},
-/**
- * 页面相关事件处理函数--监听用户下拉动作
- */
-onPullDownRefresh: function () {
-  this.getData(); // 这里写自己要调用的查询方法
-},
-onReachBottom: function () {
+  },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+    this.getData(); // 这里写自己要调用的查询方法
+  },
+  onReachBottom: function() {
     this.setData({
       curPage: this.data.curPage + 1 // 一般上拉触底是为了加载更多分页数据，所以这里页数自增
     });
@@ -112,72 +164,93 @@ onReachBottom: function () {
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    wx.cloud.database().collection('homelist')
-    .get()
-    .then(res=>{
-      console.log("获取数据成功",res)
-      this.setData({
-        datalist:res.data
+  onLoad: function(options) {
+    wx.cloud.database().collection('bookdata')
+      .get()
+      .then(res => {
+        console.log("获取数据成功", res)
+        this.setData({
+          datalist: res.data,
+        })
+        let imgList = this.data.datalist.map(obj => {
+          return obj.file_id[0]
+        })
+        this.setData({
+          imgList: imgList
+        })
+        // console.log("获取图片为",this.data.imgList)
       })
-    })
-    .catch(res=>{
-      console.log("获取数据失败",res)
-    })
-  },
-  gotodetail:function(e){
-    console.log("点击获取的数据",e)
-    wx.navigateTo({
-      url: '/pages/detail/detail?id='+e.currentTarget.dataset.item._id,
+      .catch(res => {
+        console.log("获取数据失败", res)
+      })
+    wx.cloud.database().collection('users')
+      .get()
+      .then(res => {
+        console.log("获取数据成功", res)
+        this.setData({
+          userlist: res.data,
+        })
+      })
+      .catch(res => {
+        console.log("获取数据失败", res)
+      })
+    var that = this;
+    //获取设备参数
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          imgWidth: res.windowWidth * 0.48,
+        })
+      },
     })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
